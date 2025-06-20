fabe517083cb50d81212c497777ac1d1f55dcb09
Huda <hudaayaz21@gmail.com>
2025-06-20 14:28:02 -0400

    Implemented add row button functionality

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ root.addEventListener("click", (event) => {
   console.log(event.target.tagName);
   console.log(event.target);
 });
+
+const table = document.getElementsByTagName("table")[0];
+const rows = table.rows.length;
+const columns = table.rows[0].cells.length;
+
+// Add rows to grid
+const addButton = document.getElementById("add-row");
+addButton.addEventListener("click", () => {
+  table.insertRow(rows - 1);
+  for(let i = 0; i < columns; i++) {
+    table.rows[rows - 1].insertCell(0);
+  }
+})
+
